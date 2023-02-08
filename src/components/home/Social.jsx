d5a4bbbd86b317afc979068c9f9dce64d590814b
Sana Sreeraj <iamsanasreeraj@gmail.com>
2023-02-09 01:10:01 +0530

    Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/iamsanasreeraj/" rel="noreferrer" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.instagram.com/iamsanasreeraj/" rel="noreferrer" className="home__social-icon" target="_blank">
+                <i class="uil uil-twitter-alt"></i>
+            </a>
+            
+            <a href="https://www.facebook.com/sana.sreeraj.1" rel="noreferrer" className="home__social-icon" target="_blank">
+                <i class="uil uil-facebook-f"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/sreeraj-sana-38122b250" rel="noreferrer" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            
+            <a href="https://github.com/sanasreeraj" rel="noreferrer" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
