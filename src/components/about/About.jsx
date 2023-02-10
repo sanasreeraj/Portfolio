bb2cdea6472eb52c6859673b70e3f7cd6572b631
Sana Sreeraj <iamsanasreeraj@gmail.com>
2023-02-10 20:17:20 +0530

    Skills

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ import Info from './Info';
 const About = () => {
   return (
     <section className="about section" id="about">
-      <h2 className="section__title">ABOUT ME</h2>
+      <h2 className="section__title">About Me</h2>
       <span className="section__subtitle">My Introduction</span>
 
       <div className="about__container container grid">
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Skills = () => {
+    return (
+        <section className="skills section" id="skills"></section>
+    )
+}
+
+export default Skills
